e1b54e11c24186437efe1bf13e1259a5d5ac976b
Neil Armstrong <narmstrong@baylibre.com>
2016-05-24 16:41:54 +0200

    kernel/ Changes

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -104,6 +104,8 @@ extern char core_pattern[];
 extern unsigned int core_pipe_limit;
 #endif
 extern int pid_max;
+extern int extra_free_kbytes;
+extern int min_free_order_shift;
 extern int pid_max_min, pid_max_max;
 extern int percpu_pagelist_fraction;
 extern int compat_log;
@@ -129,6 +131,7 @@ static int __maybe_unused two = 2;
 static int __maybe_unused three = 3;
 static unsigned long one_ul = 1;
 static int one_hundred = 100;
+static int mem_thresh = 65536;
 #ifdef CONFIG_PRINTK
 static int ten_thousand = 10000;
 #endif
@@ -214,6 +217,11 @@ static int sysrq_sysctl_handler(ctl_table *table, int write,
 }
 
 #endif
+
+#ifdef CONFIG_CHECK_ISR_TIME
+static int irq_time_max = INT_MAX;
+#endif
+
 
 static struct ctl_table kern_table[];
 static struct ctl_table vm_table[];
@@ -1318,6 +1326,21 @@ static struct ctl_table vm_table[] = {
 		.extra1		= &zero,
 	},
 	{
+		.procname	= "extra_free_kbytes",
+		.data		= &extra_free_kbytes,
+		.maxlen		= sizeof(extra_free_kbytes),
+		.mode		= 0644,
+		.proc_handler	= min_free_kbytes_sysctl_handler,
+		.extra1		= &zero,
+	},
+	{
+		.procname	= "min_free_order_shift",
+		.data		= &min_free_order_shift,
+		.maxlen		= sizeof(min_free_order_shift),
+		.mode		= 0644,
+		.proc_handler	= &proc_dointvec
+	},
+	{
 		.procname	= "percpu_pagelist_fraction",
 		.data		= &percpu_pagelist_fraction,
 		.maxlen		= sizeof(percpu_pagelist_fraction),
@@ -1495,6 +1518,35 @@ static struct ctl_table vm_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_doulongvec_minmax,
 	},
+	{
+		.procname	= "mem_management_thresh",
+		.data		= &mem_management_thresh,
+		.maxlen		= sizeof(mem_management_thresh),
+		.mode		= 0666,
+		.proc_handler	= proc_mem_management_thresh_handler,
+		.extra1		= &zero,
+		.extra2		= &mem_thresh,
+	},
+#ifdef CONFIG_CHECK_ISR_TIME
+	{
+		.procname	= "irq_times_stat",
+		.data		= &irq_times_stat,
+		.maxlen		= sizeof(irq_times_stat),
+		.mode		= 0666,
+		.proc_handler	= proc_irq_times_stat_handler,
+		.extra1		= &zero,
+		.extra2		= &one_hundred,
+	},
+	{
+		.procname	= "irq_times_thresh",
+		.data		= &irq_times_thresh,
+		.maxlen		= sizeof(irq_times_thresh),
+		.mode		= 0666,
+		.proc_handler	= proc_irq_times_thresh_handler,
+		.extra1		= &zero,
+		.extra2		= &irq_time_max,
+	},
+#endif
 	{ }
 };
 
