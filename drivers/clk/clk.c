62400daae8f31d68035f4c8e3ead19962b9ecde9
Jerome Brunet <jbrunet@baylibre.com>
2017-05-18 15:36:38 +0200

    clk: fix CLK_SET_RATE_GATE with clock rate protection
    Using clock rate protection, we can now enforce CLK_SET_RATE_GATE along the
    clock tree
    
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -587,6 +587,9 @@ static void clk_core_unprepare(struct clk_core *core)
 	if (WARN_ON(core->prepare_count == 1 && core->flags & CLK_IS_CRITICAL))
 		return;
 
+	if (core->flags & CLK_SET_RATE_GATE)
+		clk_core_rate_unprotect(core);
+
 	if (--core->prepare_count > 0)
 		return;
 
@@ -656,6 +659,14 @@ static int clk_core_prepare(struct clk_core *core)
 	}
 
 	core->prepare_count++;
+
+	/*
+	 * CLK_SET_RATE_GATE is a special case of clock protection
+	 * Instead of a consumer protection, the provider is protecting
+	 * itself when prepared
+	 */
+	if (core->flags & CLK_SET_RATE_GATE)
+		clk_core_rate_protect(core);
 
 	return 0;
 }
@@ -1736,9 +1747,6 @@ static int clk_core_set_rate_nolock(struct clk_core *core,
 
 	/* fail on a direct rate set of a protected provider */
 	if (clk_core_rate_is_protected(core))
-		return -EBUSY;
-
-	if ((core->flags & CLK_SET_RATE_GATE) && core->prepare_count)
 		return -EBUSY;
 
 	/* calculate new rates and get the topmost changed clock */
