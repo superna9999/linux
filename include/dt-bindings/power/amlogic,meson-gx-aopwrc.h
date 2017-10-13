7af236735c8576459a146147074b4337b90eaae7
Neil Armstrong <narmstrong@baylibre.com>
2017-10-13 15:25:13 +0200

    ARM64: dts: meson-gx: add VPU power domains

---
+++ include/dt-bindings/power/amlogic,meson-gx-aopwrc.h
@@ -0,0 +1,11 @@
+/*
+ * Amlogic Meson AO Power Domains
+ */
+
+#ifndef __GX_AOPWRC_H
+#define __GX_AOPWRC_H
+
+#define PWR_AO_VPU_HDMI		0
+#define PWR_AO_VPU_HDMI_ISO	1
+
+#endif /* __GX_AOPWRC_H */
