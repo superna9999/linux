a8ba06b8952b7d3e99a55b0f38f96d5e8af42d9d
Neil Armstrong <narmstrong@baylibre.com>
2018-11-14 17:39:46 +0100

    drm/bridge: dw-hdmi: allow ycbcr420 modes for >= 0x200a
    Now the DW-HDMI Controller supports the HDMI2.0 modes, enable support
    for these modes in the connector if the platform supports them.
    We limit these modes to DW-HDMI IP version >= 0x200a which
    are designed to support HDMI2.0 display modes.
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>

--- include/drm/bridge/dw_hdmi.h
+++ include/drm/bridge/dw_hdmi.h
@@ -127,6 +127,7 @@ struct dw_hdmi_plat_data {
 	unsigned long input_bus_format;
 	unsigned long input_bus_encoding;
 	bool use_drm_infoframe;
+	bool ycbcr_420_allowed;
 
 	/* Vendor PHY support */
 	const struct dw_hdmi_phy_ops *phy_ops;
