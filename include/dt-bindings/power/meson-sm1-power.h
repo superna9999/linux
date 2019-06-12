c627faedbc9bfe16feae76d7a4710e94b47aa2ef
Neil Armstrong <narmstrong@baylibre.com>
2019-06-12 14:39:56 +0200

    WIP: dt-bindings: power: amlogic, meson-gx-pwrc: Add SM1 bindings

---
+++ include/dt-bindings/power/meson-sm1-power.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: (GPL-2.0+ or MIT) */
+/*
+ * Copyright (c) 2019 BayLibre, SAS
+ * Author: Neil Armstrong <narmstrong@baylibre.com>
+ */
+
+#ifndef _DT_BINDINGS_MESON_SM1_POWER_H
+#define _DT_BINDINGS_MESON_SM1_POWER_H
+
+#define PWRC_USB2_PHY_ID	0
+#define PWRC_USB3_PCIE_PHY_ID	1
+
+#endif
