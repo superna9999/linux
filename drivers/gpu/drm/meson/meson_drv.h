8f1b4a85710e69135de93559da8769b02d70889e
Neil Armstrong <narmstrong@baylibre.com>
2019-08-23 12:09:23 +0200

    FROMGIT: drm: meson: use match data to detect vpu compatibility
    This patch introduce new enum which contains all VPU family (GXBB,
    GXL, GXM and G12A).
    This enum is used to detect the VPU compatible with the device.
    
    We only need to set .data to the corresponding enum in the device
    table, no need to check .compatible string anymore.
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>
    Tested-by: Neil Armstrong <narmstrong@baylibre.com>
    Acked-by: Neil Armstrong <narmstrong@baylibre.com>
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/87imqpz21w.fsf@masson.i-did-not-set--mail-host-address--so-tickle-me
    (cherry picked from commit 528a25d040bc2123fe5a9f8697a60f8bff40af0a
     git://anongit.freedesktop.org/drm/drm-misc drm-misc-next)
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>

--- drivers/gpu/drm/meson/meson_drv.h
+++ drivers/gpu/drm/meson/meson_drv.h
@@ -7,14 +7,25 @@
 #ifndef __MESON_DRV_H
 #define __MESON_DRV_H
 
+#include <linux/device.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
 #include <linux/platform_device.h>
 #include <linux/regmap.h>
 #include <linux/of.h>
 #include <linux/soc/amlogic/meson-canvas.h>
 #include <drm/drmP.h>
 
+enum vpu_compatible {
+	VPU_COMPATIBLE_GXBB = 0,
+	VPU_COMPATIBLE_GXL  = 1,
+	VPU_COMPATIBLE_GXM  = 2,
+	VPU_COMPATIBLE_G12A = 3,
+};
+
 struct meson_drm {
 	struct device *dev;
+	enum vpu_compatible compat;
 	void __iomem *io_base;
 	struct regmap *hhi;
 	int vsync_irq;
@@ -113,9 +124,9 @@ struct meson_drm {
 };
 
 static inline int meson_vpu_is_compatible(struct meson_drm *priv,
-					  const char *compat)
+					  enum vpu_compatible family)
 {
-	return of_device_is_compatible(priv->dev->of_node, compat);
+	return priv->compat == family;
 }
 
 #endif /* __MESON_DRV_H */
