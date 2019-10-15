b8884893f01e3a9264d19950307fcef4fea98669
Vandana BN <bnvandana@gmail.com>
2019-10-15 07:40:16 -0300

    BACKPORT: media: v4l2-core: Add new metadata format
    Add new metadata format to support metadata output in vivid.
    
    Signed-off-by: Vandana BN <bnvandana@gmail.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
    (cherry picked from commit 78892b6ba3ba7a6c20bc21548ee90fc980fdfbf4)
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Change-Id: I85110768d4c337adfac31759b0801ce79004ea42

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -761,6 +761,9 @@ struct v4l2_pix_format {
 #define V4L2_META_FMT_UVC         v4l2_fourcc('U', 'V', 'C', 'H') /* UVC Payload Header metadata */
 #define V4L2_META_FMT_D4XX        v4l2_fourcc('D', '4', 'X', 'X') /* D4XX Payload Header metadata */
 
+#define V4L2_META_FMT_D4XX        v4l2_fourcc('D', '4', 'X', 'X') /* D4XX Payload Header metadata */
+#define V4L2_META_FMT_VIVID	  v4l2_fourcc('V', 'I', 'V', 'D') /* Vivid Metadata */
+
 /* priv field value to indicates that subsequent fields are valid. */
 #define V4L2_PIX_FMT_PRIV_MAGIC		0xfeedcafe
 
