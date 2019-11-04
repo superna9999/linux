1231c163ff90c5b75ca6a24a189180bbc75a7935
Ricardo Ribalda Delgado <ribalda@kernel.org>
2019-11-04 14:09:18 +0100

    UPSTREAM: media: v4l2_ctrl: Add p_def to v4l2_ctrl_config
    This allows setting the default value on compound controls created via
    v4l2_ctrl_new_custom.
    
    Signed-off-by: Ricardo Ribalda Delgado <ribalda@kernel.org>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>
    (cherry picked from commit 3b98a21a3202aeb9a47c0b57cab3d3ed420c1e05)
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    
    Change-Id: I0eea12a6775f885c6c479c7ed55b35ed71e4411d

--- include/media/v4l2-ctrls.h
+++ include/media/v4l2-ctrls.h
@@ -370,6 +370,7 @@ struct v4l2_ctrl_handler {
  * @max:	The control's maximum value.
  * @step:	The control's step value for non-menu controls.
  * @def:	The control's default value.
+ * @p_def:	The control's default value for compound controls.
  * @dims:	The size of each dimension.
  * @elem_size:	The size in bytes of the control.
  * @flags:	The control's flags.
@@ -398,6 +399,7 @@ struct v4l2_ctrl_config {
 	s64 max;
 	u64 step;
 	s64 def;
+	union v4l2_ctrl_ptr p_def;
 	u32 dims[V4L2_CTRL_MAX_DIMS];
 	u32 elem_size;
 	u32 flags;
