eec5b6ff4edd2245edacec40d4b6cad6b5c7f01d
Neil Armstrong <narmstrong@baylibre.com>
2020-04-06 16:59:59 +0200

    clk: meson: g12a: DSI CTS clocks & fixups for GP0

--- drivers/clk/meson/g12a.h
+++ drivers/clk/meson/g12a.h
@@ -264,8 +264,10 @@
 #define CLKID_NNA_AXI_CLK_DIV			263
 #define CLKID_NNA_CORE_CLK_SEL			265
 #define CLKID_NNA_CORE_CLK_DIV			266
+#define CLKID_MIPI_DSI_PXCLK_DIV		268
+#define CLKID_CTS_ENCL_SEL			272
 
-#define NR_CLKS					268
+#define NR_CLKS					273
 
 /* include the CLKIDs that have been made part of the DT binding */
 #include <dt-bindings/clock/g12a-clkc.h>
--- include/dt-bindings/clock/g12a-clkc.h
+++ include/dt-bindings/clock/g12a-clkc.h
@@ -147,5 +147,8 @@
 #define CLKID_SPICC1_SCLK			261
 #define CLKID_NNA_AXI_CLK			264
 #define CLKID_NNA_CORE_CLK			267
+#define CLKID_MIPI_DSI_PXCLK_SEL		269
+#define CLKID_MIPI_DSI_PXCLK			270
+#define CLKID_CTS_ENCL				271
 
 #endif /* __G12A_CLKC_H */
