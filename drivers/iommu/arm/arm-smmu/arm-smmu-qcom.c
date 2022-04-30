50d7886554ec01d945f4b9380b6b4092cbb47743
Konrad Dybcio <konrad.dybcio@somainline.org>
2022-04-30 16:51:00 +0200

    iommu/arm-smmu-qcom: Add MSM8996 DPU compatible
    Add the MSM8996 DPU compatible to clients compatible list, as it also
    needs the workarounds.
    
    Signed-off-by: Konrad Dybcio <konrad.dybcio@somainline.org>

--- drivers/iommu/arm/arm-smmu/arm-smmu-qcom.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu-qcom.c
@@ -245,6 +245,7 @@ static const struct of_device_id qcom_smmu_client_of_match[] __maybe_unused = {
 	{ .compatible = "qcom,adreno" },
 	{ .compatible = "qcom,mdp4" },
 	{ .compatible = "qcom,mdss" },
+	{ .compatible = "qcom,msm8996-mdss" },
 	{ .compatible = "qcom,sc7180-mdss" },
 	{ .compatible = "qcom,sc7180-mss-pil" },
 	{ .compatible = "qcom,sc7280-mdss" },
