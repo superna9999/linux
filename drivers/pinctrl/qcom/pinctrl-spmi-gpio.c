d1278947e06ce155cb8d295693e2f972cd4c1dcc
Neil Armstrong <neil.armstrong@linaro.org>
2022-09-22 07:36:48 +0000

    pinctrl: qcom: spmi-gpio: add support for pmr735d gpio control
    Add support for the pmr735d gpio controller providing GPIO control over SPMI.
    
    Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>

--- drivers/pinctrl/qcom/pinctrl-spmi-gpio.c
+++ drivers/pinctrl/qcom/pinctrl-spmi-gpio.c
@@ -1242,6 +1242,7 @@ static const struct of_device_id pmic_gpio_of_match[] = {
 	{ .compatible = "qcom,pmp8074-gpio", .data = (void *) 12 },
 	{ .compatible = "qcom,pmr735a-gpio", .data = (void *) 4 },
 	{ .compatible = "qcom,pmr735b-gpio", .data = (void *) 4 },
+	{ .compatible = "qcom,pmr735d-gpio", .data = (void *) 2 },
 	/* pms405 has 12 GPIOs with holes on 1, 9, and 10 */
 	{ .compatible = "qcom,pms405-gpio", .data = (void *) 12 },
 	/* pmx55 has 11 GPIOs with holes on 3, 7, 10, 11 */
