2838a3f85f8bd3f27abc505a89268dcd0639a92f
Gaurav Kashyap <quic_gaurkash@quicinc.com>
2023-11-21 21:38:12 -0800

    qcom_scm: scm call for create, prepare and import keys
    Storage encryption has two IOCTLs for creating, importing
    and preparing keys for encryption. For wrapped keys, these
    IOCTLs need to interface with the secure environment, which
    require these SCM calls.
    
    generate_key: This is used to generate and return a longterm
                  wrapped key. Trustzone achieves this by generating
    	      a key and then wrapping it using hwkm, returning
    	      a wrapped keyblob.
    import_key:   The functionality is similar to generate, but here,
                  a raw key is imported into hwkm and a longterm wrapped
    	      keyblob is returned.
    prepare_key:  The longterm wrapped key from import or generate
                  is made further secure by rewrapping it with a per-boot
    	      ephemeral wrapped key before installing it to the linux
    	      kernel for programming to ICE.
    
    Signed-off-by: Gaurav Kashyap <quic_gaurkash@quicinc.com>
    Link: https://lore.kernel.org/r/20231122053817.3401748-8-quic_gaurkash@quicinc.com
    Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>

--- include/linux/firmware/qcom/qcom_scm.h
+++ include/linux/firmware/qcom/qcom_scm.h
@@ -105,6 +105,11 @@ int qcom_scm_ice_set_key(u32 index, const u8 *key, u32 key_size,
 			 enum qcom_scm_ice_cipher cipher, u32 data_unit_size);
 int qcom_scm_derive_sw_secret(const u8 *wkey, size_t wkey_size,
 			      u8 *sw_secret, size_t sw_secret_size);
+int qcom_scm_generate_ice_key(u8 *lt_key, size_t lt_key_size);
+int qcom_scm_prepare_ice_key(const u8 *lt_key, size_t lt_key_size,
+			     u8 *eph_key, size_t eph_size);
+int qcom_scm_import_ice_key(const u8 *imp_key, size_t imp_size,
+			    u8 *lt_key, size_t lt_key_size);
 
 bool qcom_scm_hdcp_available(void);
 int qcom_scm_hdcp_req(struct qcom_scm_hdcp_req *req, u32 req_cnt, u32 *resp);
