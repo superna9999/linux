98d2a4013a638e543c60f399355513160e63ad0f
Neil Armstrong <neil.armstrong@linaro.org>
2024-01-22 10:31:09 +0100

    DONOTMERGE: drm/msm: adreno: do not spin on GBIF_HALT_ACK on a740 GPU
    Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>

--- drivers/gpu/drm/msm/adreno/a6xx_gpu.c
+++ drivers/gpu/drm/msm/adreno/a6xx_gpu.c
@@ -1635,7 +1635,8 @@ static int hw_init(struct msm_gpu *gpu)
 	}
 
 	/* Some GPUs are stubborn and take their sweet time to unhalt GBIF! */
-	if (adreno_is_a7xx(adreno_gpu) && a6xx_has_gbif(adreno_gpu))
+	if (adreno_is_a7xx(adreno_gpu) && a6xx_has_gbif(adreno_gpu) &&
+	    !adreno_is_a740(adreno_gpu))
 		spin_until(!gpu_read(gpu, REG_A6XX_GBIF_HALT_ACK));
 
 	gpu_write(gpu, REG_A6XX_RBBM_SECVID_TSB_CNTL, 0);
