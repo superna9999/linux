94748c2dfe4b13e2833946cb042192accdfaa3d8
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2024-01-29 11:03:39 +0000

    WIP: ASoC: qcom: q6apm-dai: adjust period size to avoid a dsp bug.
    This patch is workaround for a one off bug in dsp while cacluating the size.
    Reducing the period size will avoid such addresses for buffers.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

--- sound/soc/qcom/qdsp6/q6apm-dai.c
+++ sound/soc/qcom/qdsp6/q6apm-dai.c
@@ -20,12 +20,12 @@
 
 #define PLAYBACK_MIN_NUM_PERIODS	2
 #define PLAYBACK_MAX_NUM_PERIODS	8
-#define PLAYBACK_MAX_PERIOD_SIZE	65536
-#define PLAYBACK_MIN_PERIOD_SIZE	128
+#define PLAYBACK_MAX_PERIOD_SIZE	6144
+#define PLAYBACK_MIN_PERIOD_SIZE	6144
 #define CAPTURE_MIN_NUM_PERIODS		2
 #define CAPTURE_MAX_NUM_PERIODS		8
-#define CAPTURE_MAX_PERIOD_SIZE		4096
-#define CAPTURE_MIN_PERIOD_SIZE		320
+#define CAPTURE_MAX_PERIOD_SIZE		6144
+#define CAPTURE_MIN_PERIOD_SIZE		6144
 #define BUFFER_BYTES_MAX (PLAYBACK_MAX_NUM_PERIODS * PLAYBACK_MAX_PERIOD_SIZE)
 #define BUFFER_BYTES_MIN (PLAYBACK_MIN_NUM_PERIODS * PLAYBACK_MIN_PERIOD_SIZE)
 #define COMPR_PLAYBACK_MAX_FRAGMENT_SIZE (128 * 1024)
@@ -260,6 +260,9 @@ static int q6apm_dai_prepare(struct snd_soc_component *component,
 	if (ret < 0)
 		dev_err(dev, "%s: CMD Format block failed\n", __func__);
 
+	dev_err(dev, "%s: mapping %px PCM SIZE %x periods %x \n", __func__,
+		prtd->phys, prtd->pcm_size,
+		prtd->periods);
 	ret = q6apm_map_memory_regions(prtd->graph, substream->stream, prtd->phys,
 				       (prtd->pcm_size / prtd->periods), prtd->periods);
 
@@ -396,6 +399,8 @@ static int q6apm_dai_open(struct snd_soc_component *component,
 	else
 		prtd->phys = substream->dma_buffer.addr | (pdata->sid << 32);
 
+	dev_err(component->dev, "DEBUG: %s: %px \n", __func__,
+		substream->dma_buffer.addr);
 	return 0;
 err:
 	kfree(prtd);
