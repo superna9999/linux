16adf9d6f0e0c2e46167f427d27c2922bef5c058
Neil Armstrong <neil.armstrong@linaro.org>
2024-06-21 15:47:58 +0200

    clk: qcom: dispcc-sm8650: add missing CLK_SET_RATE_PARENT flag
    Add the missing CLK_SET_RATE_PARENT for the byte0_div_clk_src
    and byte1_div_clk_src, the clock rate should propagate to
    the corresponding _clk_src.
    
    Fixes: 9e939f008338 ("clk: qcom: add the SM8650 Display Clock Controller driver")
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>

--- drivers/clk/qcom/dispcc-sm8650.c
+++ drivers/clk/qcom/dispcc-sm8650.c
@@ -696,6 +696,7 @@ static struct clk_regmap_div disp_cc_mdss_byte0_div_clk_src = {
 			&disp_cc_mdss_byte0_clk_src.clkr.hw,
 		},
 		.num_parents = 1,
+		.flags = CLK_SET_RATE_PARENT,
 		.ops = &clk_regmap_div_ops,
 	},
 };
@@ -710,6 +711,7 @@ static struct clk_regmap_div disp_cc_mdss_byte1_div_clk_src = {
 			&disp_cc_mdss_byte1_clk_src.clkr.hw,
 		},
 		.num_parents = 1,
+		.flags = CLK_SET_RATE_PARENT,
 		.ops = &clk_regmap_div_ops,
 	},
 };
