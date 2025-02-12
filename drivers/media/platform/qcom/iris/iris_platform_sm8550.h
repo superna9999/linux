3b62552783b2e8aa98e03dbbf87df59ab2a6c4e4
Neil Armstrong <neil.armstrong@linaro.org>
2025-02-12 17:57:01 +0100

    media: platform: qcom/iris: add sm8650 support
    Add support for the SM8650 platform by re-using the SM8550
    definitions and using the vpu33 ops.
    
    Move the reset tables that diffes in a per-SoC platform
    header, that will contain mode SoC specific data when
    more codecs are introduced.
    
    The SM8650/vpu33 requires more reset lines, but the H.264
    decoder capabilities are identical.
    
    Tested-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org> # x1e Dell
    Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>

---
+++ drivers/media/platform/qcom/iris/iris_platform_sm8550.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#ifndef __IRIS_PLATFORM_SM8550_H__
+#define __IRIS_PLATFORM_SM8550_H__
+
+static const char * const sm8550_clk_reset_table[] = { "bus" };
+
+#endif
