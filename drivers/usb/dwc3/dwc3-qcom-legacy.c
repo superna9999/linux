97a736a5967079382ac54898d41d0fb310a3d5ad
Bjorn Andersson <bjorn.andersson@oss.qualcomm.com>
2025-03-18 14:05:01 -0500

    usb: dwc3: qcom: Snapshot driver for backwards compatibilty
    In order to more tightly integrate the Qualcomm glue driver with the
    dwc3 core the driver is redesigned to avoid splitting the implementation
    using the driver model. But due to the strong coupling to the Devicetree
    binding needs to be updated as well.
    
    Various ways to provide backwards compatibility with existing Devicetree
    blobs has been explored, but migrating the Devicetree information
    between the old and the new binding is non-trivial.
    
    For the vast majority of boards out there, the kernel and Devicetree are
    generated and handled together, which in practice means that backwards
    compatibility needs to be managed across about 1 kernel release.
    
    For some though, such as the various Snapdragon laptops, the Devicetree
    blobs live a life separate of the kernel. In each one of these, with the
    continued extension of new features, it's recommended that users would
    upgrade their Devicetree somewhat frequently.
    
    With this in mind, simply carrying a snapshot/copy of the current driver
    is simpler than creating and maintaining the migration code.
    
    The driver is kept under the same Kconfig option, to ensure that Linux
    distributions doesn't drop USB support on these platforms.
    
    The driver, which is going to be refactored to handle the newly
    introduced qcom,snps-dwc3 compatible, is updated to temporarily not
    match against any compatible.
    
    This driver should be removed after the next LTS release.
    
    Signed-off-by: Bjorn Andersson <bjorn.andersson@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20250318-dwc3-refactor-v5-1-90ea6e5b3ba4@oss.qualcomm.com
    Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>

--- drivers/usb/dwc3/dwc3-qcom-legacy.c
+++ drivers/usb/dwc3/dwc3-qcom-legacy.c
@@ -914,6 +914,7 @@ static const struct dev_pm_ops dwc3_qcom_dev_pm_ops = {
 };
 
 static const struct of_device_id dwc3_qcom_of_match[] = {
+	{ .compatible = "qcom,dwc3" },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, dwc3_qcom_of_match);
@@ -931,4 +932,4 @@ static struct platform_driver dwc3_qcom_driver = {
 module_platform_driver(dwc3_qcom_driver);
 
 MODULE_LICENSE("GPL v2");
-MODULE_DESCRIPTION("DesignWare DWC3 QCOM Glue Driver");
+MODULE_DESCRIPTION("DesignWare DWC3 QCOM legacy glue Driver");
