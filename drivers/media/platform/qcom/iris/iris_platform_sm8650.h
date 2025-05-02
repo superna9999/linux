5de244f54d91b07766e5e5c41d26ee9d4fb67cf2
Neil Armstrong <neil.armstrong@linaro.org>
2025-05-02 09:31:27 +0200

    iris: add sm8650 preset regs
    Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>

--- drivers/media/platform/qcom/iris/iris_platform_sm8650.h
+++ drivers/media/platform/qcom/iris/iris_platform_sm8650.h
@@ -10,4 +10,20 @@ static const char * const sm8650_clk_reset_table[] = { "bus", "core" };
 
 static const char * const sm8650_controller_reset_table[] = { "xo" };
 
+static void iris_set_sm8650_preset_registers(struct iris_core *core)
+{
+	writel(0x0, core->reg_base + 0xB0088);
+	writel(0x33332222, core->reg_base + 0x13030);
+	writel(0x44444444, core->reg_base + 0x13034);
+	writel(0x1022, core->reg_base + 0x13038);
+	writel(0x0, core->reg_base + 0x13040);
+	writel(0xFFFF, core->reg_base + 0x13048);
+	writel(0x33332222, core->reg_base + 0x13430);
+	writel(0x44444444, core->reg_base + 0x13434);
+	writel(0x1022, core->reg_base + 0x13438);
+	writel(0x0, core->reg_base + 0x13440);
+	writel(0xFFFF, core->reg_base + 0x13448);
+	writel(0x99, core->reg_base + 0xA013C);
+}
+
 #endif
