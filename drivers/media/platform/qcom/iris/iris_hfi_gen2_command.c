7a92e2d621d95db965400c1acd98b1f06405432f
Neil Armstrong <neil.armstrong@linaro.org>
2025-08-20 16:59:23 +0200

    iris: fix HEVC encoding
    Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>

--- drivers/media/platform/qcom/iris/iris_hfi_gen2_command.c
+++ drivers/media/platform/qcom/iris/iris_hfi_gen2_command.c
@@ -194,9 +194,8 @@ static int iris_hfi_gen2_set_bitstream_resolution(struct iris_inst *inst, u32 pl
 		inst_hfi_gen2->src_subcr_params.bitstream_resolution = resolution;
 		payload_type = HFI_PAYLOAD_U32;
 	} else {
-		codec_align = inst->codec == V4L2_PIX_FMT_HEVC ? 32 : 16;
-		resolution = ALIGN(inst->fmt_dst->fmt.pix_mp.width, codec_align) << 16 |
-			ALIGN(inst->fmt_dst->fmt.pix_mp.height, codec_align);
+		resolution = inst->fmt_dst->fmt.pix_mp.width << 16 |
+			inst->fmt_dst->fmt.pix_mp.height;
 		inst_hfi_gen2->dst_subcr_params.bitstream_resolution = resolution;
 		payload_type = HFI_PAYLOAD_32_PACKED;
 	}
