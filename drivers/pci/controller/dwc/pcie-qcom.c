aef62b13de255a9ca5ad7a4f8d3eb9b298139e7d
Krishna Chaitanya Chundru <krishna.chundru@oss.qualcomm.com>
2026-02-17 16:49:08 +0530

    PCI: qcom: Add .get_ltssm() helper
    For older targets like sc7280, we see reading DBI after sending PME
    turn off message is causing NOC error.
    
    To avoid unsafe DBI accesses, introduce qcom_pcie_get_ltssm(), which
    retrieves the LTSSM state from the PARF_LTSSM register instead.
    
    Signed-off-by: Krishna Chaitanya Chundru <krishna.chundru@oss.qualcomm.com>
    Link: https://patch.msgid.link/20260217-d3cold-v2-3-89b322864043@oss.qualcomm.com
    Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>

--- drivers/pci/controller/dwc/pcie-qcom.c
+++ drivers/pci/controller/dwc/pcie-qcom.c
@@ -133,6 +133,7 @@
 
 /* PARF_LTSSM register fields */
 #define LTSSM_EN				BIT(8)
+#define PARF_LTSSM_STATE_MASK			GENMASK(5, 0)
 
 /* PARF_INT_ALL_{STATUS/CLEAR/MASK} register fields */
 #define PARF_INT_ALL_LINK_UP			BIT(13)
@@ -1265,6 +1266,15 @@ static bool qcom_pcie_link_up(struct dw_pcie *pci)
 	return val & PCI_EXP_LNKSTA_DLLLA;
 }
 
+static enum dw_pcie_ltssm qcom_pcie_get_ltssm(struct dw_pcie *pci)
+{
+	struct qcom_pcie *pcie = to_qcom_pcie(pci);
+	u32 val;
+
+	val = readl(pcie->parf + PARF_LTSSM);
+	return (enum dw_pcie_ltssm)FIELD_GET(PARF_LTSSM_STATE_MASK, val);
+}
+
 static void qcom_pcie_phy_power_off(struct qcom_pcie *pcie)
 {
 	struct qcom_pcie_port *port;
@@ -1497,6 +1507,7 @@ static const struct dw_pcie_ops dw_pcie_ops = {
 	.link_up = qcom_pcie_link_up,
 	.start_link = qcom_pcie_start_link,
 	.assert_perst = qcom_pcie_assert_perst,
+	.get_ltssm = qcom_pcie_get_ltssm,
 };
 
 static int qcom_pcie_icc_init(struct qcom_pcie *pcie)
