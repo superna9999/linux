97cc77eb4881f1f851492ba35a8066257f6512da
Neil Armstrong <neil.armstrong@linaro.org>
2026-04-08 16:15:33 +0200

    media: qcom: iris: add helpers for 8bit and 10bit formats
    To simplify code checking for pixel formats, add helpers to
    check for 8bit and 10bit formats.
    
    Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>

--- drivers/media/platform/qcom/iris/iris_utils.c
+++ drivers/media/platform/qcom/iris/iris_utils.c
@@ -40,6 +40,18 @@ bool iris_split_mode_enabled(struct iris_inst *inst)
 		inst->fmt_dst->fmt.pix_mp.pixelformat == V4L2_PIX_FMT_QC10C;
 }
 
+bool iris_fmt_is_8bit(__u32 pixelformat)
+{
+	return pixelformat == V4L2_PIX_FMT_NV12 ||
+		pixelformat == V4L2_PIX_FMT_QC08C;
+}
+
+bool iris_fmt_is_10bit(__u32 pixelformat)
+{
+	return pixelformat == V4L2_PIX_FMT_P010 ||
+		pixelformat == V4L2_PIX_FMT_QC10C;
+}
+
 void iris_helper_buffers_done(struct iris_inst *inst, unsigned int type,
 			      enum vb2_buffer_state state)
 {
