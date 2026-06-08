e0baa7d74305314646f672b25b32753980bd9d66
Mohammad Rafi Shaik <mohammad.rafi.shaik@oss.qualcomm.com>
2026-06-08 08:00:10 +0530

    ASoC: qcom: q6apm-lpass-dais: Add MI2S clock control
    Add support for MI2S clock control within q6apm-lpass DAIs, including
    handling of MCLK, BCLK, and ECLK via the DAI .set_sysclk callback.
    Each MI2S port now retrieves its clock handles from the device tree,
    allowing per-port clock configuration and proper enable/disable during
    startup and shutdown.
    
    Co-developed-by: Srinivas Kandagatla <srinivas.kandagatla@oss.qualcomm.com>
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@oss.qualcomm.com>
    Signed-off-by: Mohammad Rafi Shaik <mohammad.rafi.shaik@oss.qualcomm.com>
    Tested-by: Neil Armstrong <neil.armstrong@linaro.org> # on SM8650-HDK
    Link: https://patch.msgid.link/20260608023011.942228-3-mohammad.rafi.shaik@oss.qualcomm.com
    Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>

--- sound/soc/qcom/qdsp6/q6prm.h
+++ sound/soc/qcom/qdsp6/q6prm.h
@@ -3,6 +3,10 @@
 #ifndef __Q6PRM_H__
 #define __Q6PRM_H__
 
+#define LPAIF_MI2S_MCLK 1
+#define LPAIF_MI2S_BCLK 2
+#define LPAIF_MI2S_ECLK 3
+
 /* Clock ID for Primary I2S IBIT */
 #define Q6PRM_LPASS_CLK_ID_PRI_MI2S_IBIT                          0x100
 /* Clock ID for Primary I2S EBIT */
