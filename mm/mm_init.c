2f8ffea8ba1dc776cf545d722d24867a25d91b00
Muchun Song <songmuchun@bytedance.com>
2026-06-12 11:58:53 +0800

    mm/mm_init: defer hugetlb reservation until after zone initialization
    hugetlb_cma_reserve() and hugetlb_bootmem_alloc() currently run before
    free_area_init(), so HugeTLB reservation happens before zone state is
    initialized.
    
    Move the reservation step after free_area_init() so the relevant zone
    information is available before HugeTLB reserves memory.  This is needed
    for later hugetlb changes that validate boot-time HugeTLB reservations
    against zone boundaries.
    
    Link: https://lore.kernel.org/20260612035903.2468601-10-songmuchun@bytedance.com
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Reviewed-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Reviewed-by: Oscar Salvador (SUSE) <osalvador@kernel.org>
    Cc: "Aneesh Kumar K.V" <aneesh.kumar@linux.ibm.com>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Frank van der Linden <fvdl@google.com>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Madhavan Srinivasan <maddy@linux.ibm.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Nicholas Piggin <npiggin@gmail.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: "Ritesh Harjani (IBM)" <ritesh.list@gmail.com>
    Cc: Usama Arif <usama.arif@linux.dev>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/mm_init.c
+++ mm/mm_init.c
@@ -2686,10 +2686,10 @@ void __init mm_core_init_early(void)
 {
 	int nid;
 
+	free_area_init();
+
 	hugetlb_cma_reserve();
 	hugetlb_bootmem_alloc();
-
-	free_area_init();
 
 	sparse_init();
 	for_each_node_state(nid, N_MEMORY)
